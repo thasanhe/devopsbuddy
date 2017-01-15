37e3c3111ca58d54702c86656b1054ffe77d3322
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2017-01-16 00:44:25 +0530

    Created a payload page

---
+++ src/main/java/com/devopsbuddy/web/controllers/PayloadController.java
@@ -0,0 +1,17 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kingoftheroad on 1/16/17.
+ */
+@Controller
+public class PayloadController {
+    public static final String PAYLOAD_VIEW_NAME="payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+}
